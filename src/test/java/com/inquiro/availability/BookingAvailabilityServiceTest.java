53f530738bc223bb98105365bbf62ba7684a8c92
helloinquiroai-afk <hello.inquiroai@gmail.com>
2026-09-18 02:33:09 +0530

    Fix Mockito availability test stubbing

--- src/test/java/com/inquiro/availability/BookingAvailabilityServiceTest.java
+++ src/test/java/com/inquiro/availability/BookingAvailabilityServiceTest.java
@@ -41,7 +41,7 @@ class BookingAvailabilityServiceTest {
     void confirmsSlotWhenBusinessIsOpenAndNoBookingConflicts() {
         BookingAvailabilityService service = newService();
         givenBusinessIsOpen();
-        when(conflicts()).thenReturn(List.<BookingEntity>of());
+        conflicts().thenReturn(List.of());
 
         AvailabilityResult result = service.check(
                 BUSINESS_ID,
@@ -64,7 +64,7 @@ class BookingAvailabilityServiceTest {
     void rejectsSlotWhenBookingConflicts() {
         BookingAvailabilityService service = newService();
         givenBusinessIsOpen();
-        when(conflicts()).thenReturn(List.<BookingEntity>of(booking()));
+        conflicts().thenReturn(List.of(booking()));
 
         AvailabilityResult result = service.check(
                 BUSINESS_ID,
@@ -80,7 +80,7 @@ class BookingAvailabilityServiceTest {
     void cancelledBookingDoesNotBlockSlot() {
         BookingAvailabilityService service = newService();
         givenBusinessIsOpen();
-        when(conflicts()).thenReturn(List.<BookingEntity>of());
+        conflicts().thenReturn(List.of());
 
         AvailabilityResult result = service.check(
                 BUSINESS_ID,
