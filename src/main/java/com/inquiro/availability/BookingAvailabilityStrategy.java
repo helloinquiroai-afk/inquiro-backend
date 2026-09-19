a7dc5fb6907f9d96ad5265aa3055e3eec9f9e9a1
helloinquiroai-afk <hello.inquiroai@gmail.com>
2026-09-19 06:34:48 +0530

    feat: add configurable booking availability strategies

---
+++ src/main/java/com/inquiro/availability/BookingAvailabilityStrategy.java
@@ -0,0 +1,17 @@
+package com.inquiro.availability;
+
+import com.inquiro.business.BusinessProfile;
+
+import java.util.Map;
+
+public interface BookingAvailabilityStrategy {
+
+    String id();
+
+    AvailabilityResult check(
+            String businessId,
+            String service,
+            Map<String, Object> fields,
+            BusinessProfile businessProfile
+    );
+}
