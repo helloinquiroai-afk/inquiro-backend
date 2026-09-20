20395526ca6efe1d58a6dbe450abf76b650798ff
helloinquiroai-afk <hello.inquiroai@gmail.com>
2026-09-20 15:58:24 +0530

    fix: include Vite local origin in CORS

--- src/main/java/com/inquiro/config/WebConfig.java
+++ src/main/java/com/inquiro/config/WebConfig.java
@@ -22,6 +22,8 @@ public class WebConfig {
                         .allowedOrigins(
                                 "http://localhost",
                                 "http://127.0.0.1",
+                                "http://localhost:5173",
+                                "http://127.0.0.1:5173",
                                 "null",
                                 "https://inquiro.pages.dev"
                         )
